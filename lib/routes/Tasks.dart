43ee831cb2e7abfa41c44ede7258f17750154695
ZelentsovRoman <roma_zelentsov0608@mail.ru>
2023-03-08 13:41:50 +0600

    Add tasks list with description

--- lib/routes/Tasks.dart
+++ lib/routes/Tasks.dart
@@ -30,25 +30,114 @@ class _TasksState extends State<Tasks> {
         GlobalKey<RefreshIndicatorState>();
     return Scaffold(
         backgroundColor: const Color(0xFFF0F0F0),
-        body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-                child: RefreshIndicator(
-              onRefresh: getData,
-              key: _refreshIndicatorKey,
-              child: ListView.builder(
-                itemCount: list?.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('${list?[index].userId?.employee?.name}'),
-                  );
-                },
+      appBar: AppBar(
+        title: Text('Задачи'),
+        backgroundColor: Color(0xFF2D3748),
+      ),
+      drawer: Drawer(
+        backgroundColor: Color(0xFF2D3748),
+        child: Column(
+          children: const <Widget>[
+            SizedBox(
+              height: 80,
+              child: DrawerHeader(
+                padding: EdgeInsets.only(left: 0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF2D3748),
+                ),
+                child: ListTile(
+                  iconColor: Colors.white,
+                  leading: Icon(Icons.checklist, size: 40),
+                  title: Text(
+                    'TaskSystem',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
               ),
-            ))
+            ),
+            Divider(
+              height: 0,
+              color: Colors.white,
+            ),
+            ListTile(
+              iconColor: Colors.white,
+              tileColor: Colors.blueGrey,
+              leading: Icon(Icons.description, size: 40),
+              title: Text(
+                'Список задач',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            ListTile(
+              iconColor: Colors.white,
+              leading: Icon(Icons.people, size: 40),
+              title: Text(
+                'Список сотрудников',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            Expanded(child: Text('')),
+            ListTile(
+              iconColor: Colors.white,
+              leading: Icon(Icons.group_add, size: 40),
+              title: Text(
+                'Добавление сотрудника',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            ListTile(
+              iconColor: Colors.white,
+              leading: Icon(Icons.person, size: 40),
+              title: Text(
+                'Профиль',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            ListTile(
+              iconColor: Colors.white,
+              leading: Icon(Icons.logout, size: 40),
+              title: Text(
+                'Выйти',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
           ],
-        )));
+        ),
+      ),
+      body: SafeArea(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+              child: RefreshIndicator(
+            onRefresh: getData,
+            key: _refreshIndicatorKey,
+            child: ListView.builder(
+              itemCount: list?.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Text('${list?[index].taskId}'),
+                      flex: 1,
+                    ),
+                    Expanded(
+                      child: Text('${list?[index].description}'),
+                      flex: 2,
+                    ),
+                  ],
+                );
+              },
+            ),
+          ))
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xFF2D3748),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 
   Future<Null> getData() async {
